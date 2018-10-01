ea3356c8bd002d16cdda3e58cb89295246af88b4
jorge mendes <jorge.mendes75@gmail.com>
2018-10-01 14:40:49 +0100

    heroku retirar porta nao permitida

--- keystone.js
+++ keystone.js
@@ -19,7 +19,6 @@ keystone.init({
 	'favicon': 'public/favicon.ico',
 	'views': 'templates/views',
 	'view engine': 'twig',
-    'port':8080,
 	'twig options': { method: 'fs' },
 	'custom engine': Twig.render,
 	'emails': 'templates/emails',
